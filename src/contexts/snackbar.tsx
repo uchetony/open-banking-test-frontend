317b33efc095076fff80d89f6f3da017507c38e8
uchetony <uchetony007@gmail.com>
2023-01-14 07:29:06 +0100

    update snackbar to be able to stack on one another

--- src/contexts/snackbar.tsx
+++ src/contexts/snackbar.tsx
@@ -1,4 +1,5 @@
-import { Alert, AlertColor, AlertTitle, Snackbar } from "@mui/material";
+import styled from "@emotion/styled";
+import { Alert, AlertColor, AlertTitle, Snackbar, Box } from "@mui/material";
 import { nanoid } from "nanoid";
 import { useState, createContext, useContext } from "react";
 
@@ -13,6 +14,22 @@ type SnackbarDetailsWithId = SnackbarDetails & { id: string };
 type HandleDisplaySnackbar = (snackbarDetails: SnackbarDetails) => void;
 
 const SnackbarContext = createContext<HandleDisplaySnackbar | undefined>(undefined);
+
+const SnackbarContainer = styled(Box)`
+  position: fixed;
+  z-index: 999;
+  top: 50px;
+  right: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledSnackbar = styled(Snackbar)`
+  position: relative;
+  margin-bottom: 10px;
+`
 
 function SnackbarProvider({ children }: { children: JSX.Element }) {
   const [allSnackbarDetails, setAllSnackbarDetails] = useState<SnackbarDetailsWithId[]>([]);
@@ -29,25 +46,26 @@ function SnackbarProvider({ children }: { children: JSX.Element }) {
 
   return (
     <SnackbarContext.Provider value={handleDisplaySnackbar}>
-      {allSnackbarDetails.map((snackbarDetails) => (
-        <Snackbar
-          key={snackbarDetails.id}
-          open={!!snackbarDetails.message}
-          autoHideDuration={6000}
-          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-          onClose={(_, reason) => {
-            if (reason === 'clickaway') return;
-            setAllSnackbarDetails((allSnackbarDetails) => {
-              return allSnackbarDetails.filter(({ id }) => id !== snackbarDetails.id)
-            });
-          }}
-        >
-          <Alert severity={snackbarDetails?.status}>
-            { snackbarDetails?.title ? <AlertTitle>{snackbarDetails.title}</AlertTitle> : null }
-            { snackbarDetails?.message }
-          </Alert>
-        </Snackbar>
-      ))}
+      <SnackbarContainer>
+        {allSnackbarDetails.map((snackbarDetails) => (
+          <StyledSnackbar
+            key={snackbarDetails.id}
+            open={!!snackbarDetails.message}
+            autoHideDuration={6000}
+            onClose={(_, reason) => {
+              if (reason === 'clickaway') return;
+              setAllSnackbarDetails((allSnackbarDetails) => {
+                return allSnackbarDetails.filter(({ id }) => id !== snackbarDetails.id)
+              });
+            }}
+          >
+            <Alert severity={snackbarDetails?.status}>
+              { snackbarDetails?.title ? <AlertTitle>{snackbarDetails.title}</AlertTitle> : null }
+              { snackbarDetails?.message }
+            </Alert>
+          </StyledSnackbar>
+        ))}
+      </SnackbarContainer>
       {children}
     </SnackbarContext.Provider>
   )
